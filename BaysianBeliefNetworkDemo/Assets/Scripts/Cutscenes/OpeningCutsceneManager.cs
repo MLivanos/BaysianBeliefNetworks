4667677a92aa804cfc39e9d771cbccf87a4b7d06
MLivanos <59032623+MLivanos@users.noreply.github.com>
2024-12-05 20:41:06 -0800

    ✨ADD: Adds code for train scene

--- BaysianBeliefNetworkDemo/Assets/Scripts/Cutscenes/OpeningCutsceneManager.cs
+++ BaysianBeliefNetworkDemo/Assets/Scripts/Cutscenes/OpeningCutsceneManager.cs
@@ -14,9 +14,16 @@ public class OpeningCutsceneManager : MonoBehaviour
     [SerializeField] private Transform[] busDoors;
     [SerializeField] private float doorOpenSpeed;
 
+    [SerializeField] private GameObject transitScene;
+    [SerializeField] private GameObject meshGeneratorPrefab;
+    [SerializeField] private Transform train;
+    [SerializeField] private Transform cloudRing;
+    [SerializeField] private float trainSpeed;
+    [SerializeField] private float cloudRotationSpeed;
+
     private void Start()
     {
-        StartCoroutine(PlayGoodbyeScene());
+        StartCoroutine(PlayTransitScene());
     }
 
     private IEnumerator NightSkyOpener()
@@ -58,6 +65,37 @@ public class OpeningCutsceneManager : MonoBehaviour
         {
             busDoors[0].Rotate(Vector3.up * doorOpenSpeed * Time.deltaTime);
             busDoors[1].Rotate(-Vector3.up * doorOpenSpeed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    private IEnumerator PlayTransitScene()
+    {
+        mainCamera.parent = train;
+        float distanceTraveled = 0f;
+        GameObject[] meshGenerators = new GameObject[3];
+        meshGenerators[1] = Instantiate(meshGeneratorPrefab, transitScene.transform);
+        Vector3 trailingMeshGeneratorPosition = meshGenerators[1].transform.position;
+        trailingMeshGeneratorPosition.x += 49.5f;
+        Vector3 leadingMeshGeneratorPosition = meshGenerators[1].transform.position;
+        leadingMeshGeneratorPosition.x -= 49.5f;
+        meshGenerators[0] = Instantiate(meshGeneratorPrefab, trailingMeshGeneratorPosition, meshGenerators[1].transform.rotation, transitScene.transform);
+        meshGenerators[2] = Instantiate(meshGeneratorPrefab, leadingMeshGeneratorPosition, meshGenerators[1].transform.rotation, transitScene.transform);
+        while(true)
+        {
+            train.Translate(Vector3.back * trainSpeed * Time.deltaTime);
+            cloudRing.Rotate(Vector3.up * cloudRotationSpeed * Time.deltaTime);
+            distanceTraveled += trainSpeed * Time.deltaTime;
+            if (distanceTraveled >= 50f)
+            {
+                Destroy(meshGenerators[0]);
+                meshGenerators[0] = meshGenerators[1];
+                meshGenerators[1] = meshGenerators[2];
+                Vector3 newMeshPosition = meshGenerators[1].transform.position;
+                newMeshPosition.x -= 49.5f;
+                meshGenerators[2] = Instantiate(meshGeneratorPrefab, newMeshPosition, meshGenerators[1].transform.rotation);
+                distanceTraveled -= 49.5f;
+            }
             yield return null;
         }
     }
