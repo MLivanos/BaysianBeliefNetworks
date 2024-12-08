4caed0691cac84170c6ec3f83b2f7260cf5282fb
MLivanos <59032623+MLivanos@users.noreply.github.com>
2024-12-08 12:47:10 -0800

    🚚RENAME: Renames OpeningCutSceneManager to CutSceneManager

--- BaysianBeliefNetworkDemo/Assets/Scripts/Cutscenes/CutsceneManager.cs
+++ BaysianBeliefNetworkDemo/Assets/Scripts/Cutscenes/CutsceneManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OpeningCutsceneManager : MonoBehaviour
+public class CutsceneManager : MonoBehaviour
 {
     [SerializeField] private CutsceneBehavior[] cutscenes;
     [SerializeField] private TypewriterEffect typewriterEffect;
