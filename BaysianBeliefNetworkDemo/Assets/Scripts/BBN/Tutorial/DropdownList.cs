516058a65679ac750c18be1faa1db53155d5a8ea
MLivanos <59032623+MLivanos@users.noreply.github.com>
2025-01-20 15:55:43 -0800

    ✨ADD: Adds dropdown list for tutorial steps

---
+++ BaysianBeliefNetworkDemo/Assets/Scripts/BBN/Tutorial/DropdownList.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using UnityEngine;
+
+public class DropdownList : MonoBehaviour
+{
+    [SerializeField] private Transform panelTransform;
+    [SerializeField] private Vector3 endPosition;
+    [SerializeField] private float animationSpeed = 5f;
+
+    private Vector3 startPosition;
+    private Coroutine currentCoroutine;
+
+    private void Start()
+    {
+        startPosition = panelTransform.localPosition;
+    }
+
+    public void MoveUp()
+    {
+        StartMovePanelCoroutine(false);
+    }
+
+    public void MoveDown()
+    {
+        StartMovePanelCoroutine(true);
+    }
+
+    private void StartMovePanelCoroutine(bool movingDown)
+    {
+        if (currentCoroutine != null) StopCoroutine(currentCoroutine);
+        currentCoroutine = StartCoroutine(MovePanel(movingDown));
+    }
+
+    private IEnumerator MovePanel(bool movingDown)
+    {
+        Vector3 initialPosition = panelTransform.localPosition;
+        Vector3 targetPosition = movingDown ? endPosition : startPosition;
+
+        float totalDistance = Vector3.Distance(initialPosition, targetPosition);
+        float totalTime = totalDistance / animationSpeed;
+
+        float timer = 0f;
+        while (timer < totalTime)
+        {
+            panelTransform.localPosition = Vector3.Lerp(initialPosition, targetPosition, timer / totalTime);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        panelTransform.localPosition = targetPosition;
+        currentCoroutine = null;
+    }
+}
