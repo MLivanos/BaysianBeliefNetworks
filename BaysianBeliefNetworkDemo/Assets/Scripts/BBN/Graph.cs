e23e565ee46cf07bb540737edd0757300531f4d8
MLivanos <59032623+MLivanos@users.noreply.github.com>
2025-03-30 12:21:34 -0700

    ✨ADD: Adds history scrollbar and text component

--- BaysianBeliefNetworkDemo/Assets/Scripts/BBN/Graph.cs
+++ BaysianBeliefNetworkDemo/Assets/Scripts/BBN/Graph.cs
@@ -14,6 +14,7 @@ public class Graph : MonoBehaviour
     [SerializeField] private GameObject gibbsOptions;
     [SerializeField] private GameObject hamiltonianOptions;
     [SerializeField] private GameObject graphUI;
+    [SerializeField] private SamplingHistory queryRecord;
     [SerializeField] private bool test;
     private GraphUIManager graphUIManager;
     private AudioManager audioManager;
@@ -25,7 +26,6 @@ public class Graph : MonoBehaviour
     private List<Node> negativeQuery;
     private List<Node> positiveEvidence;
     private List<Node> negativeEvidence;
-    private SamplingHistory queryRecord;
     bool isNegative;
     float lastProbability;
 
@@ -50,7 +50,6 @@ public class Graph : MonoBehaviour
         samplers[2] = GetComponent<GibbsSampler>();
         samplers[3] = GetComponent<HamiltonianSampler>();
         graphUIManager = GetComponent<GraphUIManager>();
-        queryRecord = GetComponent<SamplingHistory>();
         currentSampler = samplers[0];
     }
 
