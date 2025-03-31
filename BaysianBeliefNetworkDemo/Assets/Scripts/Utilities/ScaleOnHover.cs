6448e75c879caf7d08a6781863c9e4644bdd0d68
MLivanos <59032623+MLivanos@users.noreply.github.com>
2025-03-31 13:54:51 -0700

    ✨ADD: Adds system for scaling UI elements when hovering

---
+++ BaysianBeliefNetworkDemo/Assets/Scripts/Utilities/ScaleOnHover.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using UnityEngine;
+
+public class ScaleOnHover : MonoBehaviour, IXRaycastTargetScript
+{
+    [SerializeField] private Vector2 hoverScale = new Vector2(1.05f, 1.05f);
+    [SerializeField] private float scaleSpeed = 8f;
+
+    private Vector3 originalScale;
+    private Coroutine scalingCoroutine;
+
+    private void Awake()
+    {
+        originalScale = transform.localScale;
+        hoverScale *= originalScale;
+    }
+
+    public void OnDeepPointerEnter()
+    {
+        StartScaleTween(hoverScale);
+    }
+
+    public void OnDeepPointerExit()
+    {
+        StartScaleTween(new Vector2(originalScale.x, originalScale.y));
+    }
+
+    private void StartScaleTween(Vector2 target)
+    {
+        if (scalingCoroutine != null)
+            StopCoroutine(scalingCoroutine);
+        scalingCoroutine = StartCoroutine(ScaleTo(new Vector3(target.x, target.y, originalScale.z)));
+    }
+
+    private IEnumerator ScaleTo(Vector3 targetScale)
+    {
+        Vector3 start = transform.localScale;
+        float progress = 0f;
+
+        while (progress < 1f)
+        {
+            progress += Time.deltaTime * scaleSpeed;
+            transform.localScale = Vector3.Lerp(start, targetScale, progress);
+            yield return null;
+        }
+
+        transform.localScale = targetScale;
+        scalingCoroutine = null;
+    }
+}
