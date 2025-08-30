e9ffb2ec38190f5c50843965bb9018397e63c18a
MLivanos <59032623+MLivanos@users.noreply.github.com>
2025-08-30 08:02:02 -1000

    🐛FIX: Fixes BBN to interviews transition

--- BaysianBeliefNetworkDemo/Assets/Scripts/BBN/TransitionToInterviews.cs
+++ BaysianBeliefNetworkDemo/Assets/Scripts/BBN/TransitionToInterviews.cs
@@ -3,62 +3,81 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
 public class TransitionToInterviews : Transition
 {
-	[SerializeField] private GameObject background;
-	[SerializeField] private GameObject reflection;
-	[SerializeField] private RawImage whiteToDot;
-	[SerializeField] private GameObject circleToDot;
-	[SerializeField] private float minHeight;
-	[SerializeField] private float timeBetweenWhiteAndDot;
-	[SerializeField] private float whiteToLineTime;
-	[SerializeField] private float lineToDotTime;
-	private float maxHeight;
-	private float maxWidth;
+    [SerializeField] private GameObject background;
+    [SerializeField] private GameObject reflection;
+    [SerializeField] private RawImage whiteToDot;
+    [SerializeField] private GameObject circleToDot;
+    [SerializeField] private float minHeight = 2f;
+    [SerializeField] private float timeBetweenWhiteAndDot = 0.1f;
+    [SerializeField] private float whiteToLineTime = 0.25f;
+    [SerializeField] private float lineToDotTime = 0.25f;
 
-	protected override IEnumerator TransitionToScene()
+    float maxHeight, maxWidth;
+    RectTransform rt;
+
+    protected override IEnumerator TransitionToScene()
     {
-    	maxHeight = whiteToDot.rectTransform.sizeDelta.y;
-    	maxWidth = whiteToDot.rectTransform.sizeDelta.x;
-    	ShowObjects();
-    	yield return BringWhiteToLine();
-    	yield return new WaitForSeconds(timeBetweenWhiteAndDot);
-    	yield return BringLineToDot();
-    	sceneManager.GoToInterviews();
+        rt = whiteToDot.rectTransform;
+
+        // Wait one frame so layout/CanvasScaler has correct sizes
+        yield return null;
+        maxHeight = rt.rect.height;
+        maxWidth  = rt.rect.width;
+
+        ShowObjects();
+
+        yield return BringWhiteToLine();
+        yield return new WaitForSeconds(timeBetweenWhiteAndDot);
+        yield return BringLineToDot();
+
+        sceneManager.GoToInterviews();
     }
 
-    private void ShowObjects()
+    void ShowObjects()
     {
-    	background.SetActive(true);
-    	reflection.SetActive(true);
-    	whiteToDot.gameObject.SetActive(true);
-    	circleToDot.SetActive(true);
+        background.SetActive(true);
+        reflection.SetActive(true);
+        whiteToDot.gameObject.SetActive(true);
+        circleToDot.SetActive(true);
     }
 
-    private IEnumerator BringWhiteToLine()
+    IEnumerator BringWhiteToLine()
     {
-    	float timer = 0f;
-    	float height = 0f;
-    	while(timer < whiteToLineTime)
-    	{
-    		height = (maxHeight - minHeight) * (1f-timer / whiteToLineTime) + minHeight;
-    		whiteToDot.rectTransform.sizeDelta = new Vector2(maxWidth, height);
-    		timer += Time.deltaTime;
-    		yield return null;
-    	}
+        float t = 0f;
+        while (t < whiteToLineTime)
+        {
+            float height = Mathf.Lerp(maxHeight, minHeight, t / whiteToLineTime);
+
+            rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, maxWidth);
+            rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical,   height);
+
+            t += Time.deltaTime;
+            yield return null;
+        }
+        rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, minHeight);
     }
 
-    private IEnumerator BringLineToDot()
+    IEnumerator BringLineToDot()
     {
-    	float timer = 0f;
-    	float width = 0f;
-    	while(timer < lineToDotTime)
-    	{
-    		circleToDot.transform.localScale = Vector3.one*(1f-timer / lineToDotTime);
-    		width = maxWidth * (1f-timer / lineToDotTime);
-    		whiteToDot.rectTransform.sizeDelta = new Vector2(width, minHeight);
-    		timer += Time.deltaTime;
-    		yield return null;
-    	}
+        float t = 0f;
+        while (t < lineToDotTime)
+        {
+            float k = t / lineToDotTime;
+
+            circleToDot.transform.localScale = Vector3.one * (1f - k);
+            float width = Mathf.Lerp(maxWidth, 0f, k);
+            rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, width);
+            rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical,   minHeight);
+
+            t += Time.deltaTime;
+            yield return null;
+        }
+        rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 0f);
     }
 }
